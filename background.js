b438b253aed12779c0401a53d925a7be1c1df90f
AmiR <www.devmoon@gmail.com>
2025-04-15 14:44:26 +0330

    Add files via upload

---
+++ background.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  VANTA.NET({
+    el: "#background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 100.00,
+    minWidth: 100.00,
+    scale: 1.00,
+    scaleMobile: 0.5,
+    color: 0xff1a1a,           
+    backgroundColor: 0x1a1a1a,  
+    points: 15.00,
+    maxDistance: 15.00,
+    spacing: 18.00
+  });
+});
+
+
